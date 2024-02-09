ab9714e27730b05b1acf8ed65af129f450794f88
Mihail <miha_alekseev_2003@bk.ru>
2024-02-09 19:55:54 +0300

    changed main

---
+++ Лабораторная 1/#Project#/attacker.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+
+def timed_checker(files):
+    newfiles = os.listdir("Public")
+    if newfiles != files:
+        for file in newfiles:
+            if not(file in newfiles):
+                print(f"Created new file:{file}!")
+    return newfiles
+
+if __name__ == '__main__':
+    files = os.listdir("Public")
+    mode = input("ВЫберете что хотите сделать:\n1. Мониторинг директории Publiс\
+                 \n2. Разовая проверка\nДля введите что-то иное\n")
+    if mode == "1":
+        while 1:
+            flag = 0
+            files = timed_checker(files)
+            print(files)
+            
+        
+    
+
