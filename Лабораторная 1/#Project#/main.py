bf66799776e642bb44bb90f99e81917acd61483e
Paul Cher <pashca007@mail.ru>
2024-02-09 19:38:27 +0300

    Added Welcome message and slightly changed interface

--- Лабораторная 1/#Project#/main.py
+++ Лабораторная 1/#Project#/main.py
@@ -3,18 +3,19 @@ import shutil
 
 
 def create_file(file_name):
-    direct = "Private"
+    direct = "..//Private"
     file = open(direct+"\\"+file_name, 'w+')
     file.write("Hello World!!!!!!!!!!!!!!!!")
+    print(f"Файл {file_name} был успешно создан!\n")
     file.close()
     return 0
 
 
 def move_file(file_name, mode):
-    files = os.listdir("Private")
+    files = os.listdir("../Private")
 
     if mode == "1":
-        shutil.copytree("Private", "Public", dirs_exist_ok = True)
+        shutil.copytree("..//Private", "..//Public", dirs_exist_ok = True)
         return 0
     elif mode == "0":
         idFile = input("Введите номер файла: ")
@@ -22,7 +23,7 @@ def move_file(file_name, mode):
             print("Неверный номер файла")
             return -1
         if int(idFile) <= len(files):
-            shutil.copy("Private\\"+files[int(idFile)-1], "Public")
+            shutil.copy("..//Private\\"+files[int(idFile)-1], "..//Public")
         else:
             print("Некоректный номер файла")
         return 0
@@ -33,17 +34,21 @@ def move_file(file_name, mode):
 
 
 if __name__ == '__main__':
-
+    columns = shutil.get_terminal_size().columns
+    print("==========".center(columns))
+    print("Добро пожаловать в программу для создания и копирования файлов!".center(columns))
+    print("==========\n".center(columns))
     while True:
-        print("Введите операцию:")
+        columns = shutil.get_terminal_size().columns
 
-        mode = input("1. Создать файл\n2. Перенести файлы в общую папку\n0. Выйти из программы\n")
 
+        print("1. Создать файл\n2. Перенести файлы в общую папку\n0. Выйти из программы\n")
+        mode =input("Введите операцию: ")
         if mode == "1":
             path = input("Введите имя файла: ")
             create_file(path+".txt")
         elif mode == "2":
-            files = os.listdir("Private")
+            files = os.listdir("..//Private")
             i = 1
             for file in files:
                 print(f"{i}. {file}")
