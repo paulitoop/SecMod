a930e35813fa9efbf9322977efb20f562b7549d9
Paul Cher <pashca007@mail.ru>
2024-02-10 20:46:51 +0300

    Created main window

--- Лабораторная 1/#Project#/main.py
+++ Лабораторная 1/#Project#/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
-
-
+from tkinter import *
+from tkinter import ttk
 def create_file(file_name):
     direct = "..//Private"
     file = open(direct+"\\"+file_name, 'w+')
@@ -36,36 +36,52 @@ def move_file(file_name, mode):
 
 
 if __name__ == '__main__':
-    columns = shutil.get_terminal_size().columns
-    print("==========".center(columns))
-    print("Добро пожаловать в программу для создания и копирования файлов!".center(columns))
-    print("==========".center(columns))
-    while True:
-        columns = shutil.get_terminal_size().columns
 
-        print("\n-----")
-        print("1. Создать файл\n2. Перенести файлы в общую папку\n0. Выйти из программы\n")
-        mode =input("Введите операцию: ")
-        print()
-        if mode == "1":
-            path = input("Введите имя файла: ")
-            create_file(path+".txt")
-        elif mode == "2":
-            files = os.listdir("..//Private")
-            i = 1
+    mw = Tk()
+    mw.title("Users's programm")
+    mw.geometry('700x300+400+200')
+    mw.configure(background="#66CDAA")
+    mw.resizable(False, False)
+    mw.iconbitmap(default="userIco.ico")
 
-            print("Список файлов:")
-            for file in files:
+    for i in range(3): mw.columnconfigure(i,weight=1)
+    for i in range(8): mw.rowconfigure(i, weight=1)
+    label1 = Label(text = "Hello world!")
+    label1.pack()
 
-                print(f"{i}. {file}")
-                i+=1
-            print("\n-----")
-            flag = input("Введите параметр переноса:\n1. Выбрать файл\n2. Перенести все\n")
-            move_file("test.txt", flag)
-        elif mode == "0":
-            break
-        else:
-            print("Некорректная операция!\n")
-            continue
+    mw.mainloop()
+
+
+    # columns = shutil.get_terminal_size().columns
+    # print("==========".center(columns))
+    # print("Добро пожаловать в программу для создания и копирования файлов!".center(columns))
+    # print("==========".center(columns))
+    # while True:
+    #     columns = shutil.get_terminal_size().columns
+    #
+    #     print("\n-----")
+    #     print("1. Создать файл\n2. Перенести файлы в общую папку\n0. Выйти из программы\n")
+    #     mode =input("Введите операцию: ")
+    #     print()
+    #     if mode == "1":
+    #         path = input("Введите имя файла: ")
+    #         create_file(path+".txt")
+    #     elif mode == "2":
+    #         files = os.listdir("..//Private")
+    #         i = 1
+    #
+    #         print("Список файлов:")
+    #         for file in files:
+    #
+    #             print(f"{i}. {file}")
+    #             i+=1
+    #         print("\n-----")
+    #         flag = input("Введите параметр переноса:\n1. Выбрать файл\n2. Перенести все\n")
+    #         move_file("test.txt", flag)
+    #     elif mode == "0":
+    #         break
+    #     else:
+    #         print("Некорректная операция!\n")
+    #         continue
 
 
