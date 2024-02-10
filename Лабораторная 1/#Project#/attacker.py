37a6dfe15cead3d9f0dd2dc9a92c3ee8f1c4801b
Mihail <miha_alekseev_2003@bk.ru>
2024-02-10 13:34:56 +0300

    added moved file filename

--- Лабораторная 1/#Project#/attacker.py
+++ Лабораторная 1/#Project#/attacker.py
@@ -6,15 +6,19 @@ def timed_checker(files):
     newfiles = os.listdir("Public")
     if newfiles != files:
         print("Created new file:")
-        for file in newfiles:
-            if not(file in newfiles):
-                print(f"Created new file:{file}!")
+        for nfile in newfiles:
+            flag = 0
+            for file in files:
+                if nfile == file:
+                    flag = 1
+            if flag == 0:
+                print(f"New file is:{nfile}")
     return newfiles
 
 if __name__ == '__main__':
     files = os.listdir("Public")
     mode = input("ВЫберете что хотите сделать:\n1. Мониторинг директории Publiс\
-                 \n2. Разовая проверка\nДля введите что-то иное\n")
+                 \n2. Разовая проверка\nДля выхода введите что-то иное\n")
     if mode == "1":
         while 1:
             flag = 0
