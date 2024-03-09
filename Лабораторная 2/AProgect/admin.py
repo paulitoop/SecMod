cb1ddf641774e32b8bab9477ca9ffeb0da913fff
Mihail <miha_alekseev_2003@bk.ru>
2024-03-09 19:41:39 +0300

    admin(worked)

--- Лабораторная 2/AProgect/admin.py
+++ Лабораторная 2/AProgect/admin.py
@@ -1,18 +1,117 @@
 import os
 import shutil
 from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import collections
 
+def get_matrix(path):
+    f = open(path)
+    buf = f.read()
+    m = 0
+    if buf!= "" and '\n' in buf:
+        m = buf.split('\n')
+    f.close()
+    return m
+
+def print_matrix(d):
+    matrix.delete("1.0", END)
+    alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxwz0123456789"
+    maxs = 0
+    for s in d:
+        if len(s)> maxs:
+            maxs = len(s)
+
+    matrix.insert (END, " "*maxs)
+    for symb in alf:
+        matrix.insert(END, symb+" ")
+    matrix.insert(END, "\n")
+    for users in d:
+        matrix.insert(END, users)
+        if len(users)< maxs:
+            matrix.insert(END, " "*(maxs-len(users)))
+        for i in alf:
+            if i in d[users]:
+                matrix.insert(END, "+ ")
+            else:
+                matrix.insert(END, "- ")
+        matrix.insert(END, "\n")
+            
+def set_dict(m):
+    d = dict()
+    for s in m:
+        a, b = s.split("-")
+        d[a] = sorted(set(b))
+    return d
+
+def update():
+    m = get_matrix("matrix.txt")
+    if m != 0:
+        d = set_dict(m)
+        print_matrix(d)
+    return 0
+
+def change_roots(u,r):
+    m = get_matrix("matrix.txt")
+    d = dict()
+    f = open("matrix.txt", 'w')
+    if m != 0:
+        d = set_dict(m)
+        print(d)
+        d[u] = set(sorted(r))
+        for users in d:
+            f.write(str(users)+"-")
+            for i in d[users]:
+                f.write(str(i))
+            [last] = collections.deque(d, maxlen=1)
+            if users != last:
+                f.write('\n')
+        f.close()
+        update()
+        
+    else:
+        return -1
+    return 0
 if __name__ == '__main__':
     
     window = Tk()  
     window.title("Admin's window")  
-    window.geometry('700x700')
-    window.configure(bg='#4B0082')
+    window.geometry('600x500')
+    window.configure(bg='#2B2381')
     window.resizable(width=False, height=False)
 
     for i in range(3): window.columnconfigure(index=i, weight=1)
-    for i in range(8): window.rowconfigure(index=i, weight=1)
+    for i in range(15): window.rowconfigure(index=i, weight=1)
+
+    txt_frm = tk.Frame(window, width=150, height=150)
+    txt_frm.grid(row=1, column=0, rowspan= 1, columnspan=3)
+    
+    mat_text = tk.Label(text="Матрица доступов",height=2, width=30)
+    mat_text.grid(row = 0, column=1, columnspan= 1, rowspan=1)
+
+    matrix = tk.Text(txt_frm, borderwidth=3, relief="sunken",height=10, width=60)
+    matrix.config(font=("consolas", 12), undo=True, wrap=NONE)
+    matrix.grid(row=0, column=0, sticky="nsew", padx=1, pady=1)
+
+    scrollb = tk.Scrollbar(txt_frm,orient=HORIZONTAL, command=matrix.xview)
+    scrollb.grid(row=1, column=0, sticky='nsew')
+    matrix['xscrollcommand'] = scrollb.set
+
+    scrollby = tk.Scrollbar(txt_frm, orient=VERTICAL, command=matrix.yview)
+    scrollby.grid(row=0, column=1, sticky='nsew')
+    matrix['yscrollcommand'] = scrollby.set    
+
+    update()
+    addBtn = tk.Button(text = "Изменить права",height=1, width=20, command=lambda: change_roots(entryLogin.get(), entryRoot.get()))
+    addBtn.grid(row =4, column=1, columnspan= 1, rowspan=1)
+
+    tx1 = tk.Label(text="Имя пользователя",height=2, width=20)
+    tx1.grid(row = 3, column=0 ,rowspan=1,columnspan=1)
+    entryLogin = Entry(width = 20)
+    entryLogin.grid(row = 4, column=0 ,rowspan=1,columnspan=1)
+    tx2 = tk.Label(text="Новые права",height=2, width=20)
+    tx2.grid(row = 3, column=2 ,rowspan=1,columnspan=1)
+    entryRoot = Entry(width = 20)
+    entryRoot.grid(row =4, column=2,rowspan=1,columnspan=1)
 
     mainloop()
     
